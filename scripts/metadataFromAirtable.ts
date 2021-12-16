ba04c4f01f3f9b0f4e4f9be75970a0b37826d352
Jim Zheng <jim32990@gmail.com>
2021-12-16 12:08:12 -0500

    pull from airtable

---
+++ scripts/metadataFromAirtable.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+
+import Airtable from 'airtable'
+import { Attachment } from 'airtable'
+import { AirtableBase } from 'airtable/lib/airtable_base'
+
+dotenv.config({ path: '.env.local' })
+
+function getAirtableClient(baseId: string): AirtableBase {
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+  })
+  const base = Airtable.base(baseId)
+  return base
+}
+
+// eslint-disable-next-line prettier/prettier
+; (async (connection) => {
+  console.log('Connecting to Airtable')
+  const base = getAirtableClient('appn2XbAF0nNuS6de')
+
+  await base('Table 1')
+    .select({
+      pageSize: 3,
+      view: 'Grid view'
+    })
+    .eachPage(
+      async (records, fetchNextPage) => {
+        records.forEach(async (record) => {
+          const tokenId = record.fields['token_id'] as string
+          const name = record.fields['name'] as string
+          const images = record.fields['image'] as Attachment[]
+          // ...
+        })
+
+        fetchNextPage()
+      },
+      (err: Error) => {
+        if (err) console.error(err)
+      }
+    )
+})()
