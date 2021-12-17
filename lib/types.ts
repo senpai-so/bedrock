adb3e0672327ce14e95fd63ebf3a477978a625d0
Jim Zheng <jim32990@gmail.com>
2021-12-17 16:56:11 -0500

    add view by token id page

--- lib/types.ts
+++ lib/types.ts
@@ -1,2 +1,26 @@
 // because ts compiler complains about using empty `{}` in props
 export type EmptyProps = Record<string, unknown>
+
+export interface NFTContract {
+  contract: string
+  symbol: string
+  name: string
+  icon: string
+  homepage: string
+  marketplace: string[]
+}
+
+export interface NFTMarketplace {
+  link: string
+  name: string
+}
+
+export interface NFTTokenItem {
+  extension: Extension
+}
+
+export interface Extension {
+  name?: string
+  description?: string
+  image?: string
+}
