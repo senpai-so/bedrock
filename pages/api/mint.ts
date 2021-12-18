6747fffd2107eda669b6c620e866c808cc82aa52
Eunice Tan <eunicehx920@gmail.com>
2021-12-18 12:07:38 -0800

    ok fixed formatting

--- pages/api/mint.ts
+++ pages/api/mint.ts
@@ -1,82 +1,82 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { MnemonicKey, MsgExecuteContract } from "@terra-money/terra.js";
+import { MnemonicKey, MsgExecuteContract } from '@terra-money/terra.js'
 
-import { getLCD } from "../../lib/utils/terra";
-import { mnemonic, ownerAddress, contractAddress } from "../../lib/config";
-import { toULuna } from "../../lib/utils/currency";
+import { getLCD } from '../../lib/utils/terra'
+import { mnemonic, ownerAddress, contractAddress } from '../../lib/config'
+import { toULuna } from '../../lib/utils/currency'
 
-import prisma from "../../lib/prisma";
-import pickRandom from "pick-random";
+import prisma from '../../lib/prisma'
+import pickRandom from 'pick-random'
 
 type SwapResponse = {
-  success: boolean;
-  error?: string;
-};
+  success: boolean
+  error?: string
+}
 
 // Prisma schema
 interface NftTokens {
-  id: number;
-  token_id: string;
-  name: string;
-  description: string;
-  extension_name: string;
-  extension_image: string;
-  image_uri: string;
-  isMinted: () => boolean;
+  id: number
+  token_id: string
+  name: string
+  description: string
+  extension_name: string
+  extension_image: string
+  image_uri: string
+  isMinted: () => boolean
 }
 
 async function get_random_non_minted_nft() {
   const tokensAvailable: NftTokens[] = (await prisma.nftTokens.findMany({
     where: { isMinted: false },
-    select: { id: true },
-  })) as NftTokens[];
+    select: { id: true }
+  })) as NftTokens[]
 
-  const random_token = pickRandom(tokensAvailable)[0];
+  const random_token = pickRandom(tokensAvailable)[0]
   const full_token = await prisma.nftTokens.findUnique({
-    where: { id: random_token.id },
-  });
-  return full_token;
+    where: { id: random_token.id }
+  })
+  return full_token
 }
 
 async function save_mint_to_db(token_id?: string) {
   if (token_id) {
     await prisma.nftTokens.update({
       where: { token_id: token_id },
-      data: { isMinted: true },
-    });
+      data: { isMinted: true }
+    })
   }
-  return;
+  return
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<SwapResponse>
 ) {
-  if (req.method === "POST") {
-    const { buyer } = req.body;
+  if (req.method === 'POST') {
+    const { buyer } = req.body
 
     if (!mnemonic) {
-      console.log("Environment variable `SIGNER_WALLET_MNEMONIC` is not set");
+      console.log('Environment variable `SIGNER_WALLET_MNEMONIC` is not set')
 
       res.status(500).json({
         success: false,
-        error: "Service down for maintenance. Please try again later.",
-      });
+        error: 'Service down for maintenance. Please try again later.'
+      })
 
-      return;
+      return
     }
 
-    const token = await get_random_non_minted_nft();
+    const token = await get_random_non_minted_nft()
 
-    console.log(token);
-    const lcd = await getLCD();
-    const mk = new MnemonicKey({ mnemonic });
-    const signer = lcd.wallet(mk);
-    console.log("signer", signer);
+    console.log(token)
+    const lcd = await getLCD()
+    const mk = new MnemonicKey({ mnemonic })
+    const signer = lcd.wallet(mk)
+    console.log('Signer', signer)
 
-    console.log("ownerAddress", ownerAddress);
-    console.log("contractAddress", contractAddress);
+    console.log('ownerAddress', ownerAddress)
+    console.log('contractAddress', contractAddress)
 
     const msg = new MsgExecuteContract(
       ownerAddress,
@@ -90,39 +90,39 @@ export default async function handler(
           image: token?.image_uri,
           extension: {
             name: token?.extension_name,
-            image: token?.extension_image,
-          },
-        },
+            image: token?.extension_image
+          }
+        }
       },
       // TODO use the correct gas price
       { uluna: toULuna(1) }
-    );
+    )
 
-    console.log(msg);
+    console.log(msg)
 
     // mint part
     const tx = await signer
       .createAndSignTx({
-        msgs: [msg],
+        msgs: [msg]
       })
       .catch((error: unknown) => {
-        console.log("Error creating and signing transaction");
-        console.log(error);
-        throw error;
-      });
+        console.log('Error creating and signing transaction')
+        console.log(error)
+        throw error
+      })
 
-    console.log("mint tx", tx);
-    const result = await lcd.tx.broadcast(tx);
-    console.log(result);
+    console.log('mint tx', tx)
+    const result = await lcd.tx.broadcast(tx)
+    console.log(result)
 
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true })
 
-    await save_mint_to_db(token?.token_id);
+    await save_mint_to_db(token?.token_id)
 
-    return;
+    return
   }
 
   // catch-all
-  res.status(404).json({ success: false, error: "NOT FOUND" });
-  return;
+  res.status(404).json({ success: false, error: 'NOT FOUND' })
+  return
 }
