f324db2bbdf7746fe4c36cd0262a3dd60673613b
Eunice Tan <eunicehx920@gmail.com>
2021-12-20 02:24:58 -0800

    where is the env coming from

--- lib/utils/terra.ts
+++ lib/utils/terra.ts
@@ -39,6 +39,8 @@ export async function getLCD() {
   console.log('is preview', isPreview)
   console.log(process.env.NEXT_IS_PREVIEW)
   console.log(process.env.NODE_ENV)
+  console.log(process.env.NEXT_PUBLIC_VERCEL_ENV)
+  console.log(process.env.VERCEL_ENV)
   const lcdUrl = getLcdUrl()
   const chainID = getChainID()
   const gasPrices = await getGasPrices()
