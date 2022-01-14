bc2c18c6f3367cd19d375cb4e8a8867335ce57c7
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-01-14 16:10:30 -0600

    Abstract contract msg sending

---
+++ packages/cli/src/utils/contract.ts
@@ -0,0 +1,26 @@
+import { isTxError, LCDClient, MsgExecuteContract, Wallet } from "@terra-money/terra.js"
+
+
+export const executeTransaction = async (
+  terra: LCDClient,
+  wallet: Wallet,
+  contract_address: string,
+  execMsg: Object,
+) => {
+  const execute = new MsgExecuteContract(
+    wallet.key.accAddress, 
+    contract_address, 
+    execMsg
+  );
+  const executeTx = await wallet.createAndSignTx({ msgs: [execute] });
+  const executeTxResult = await terra.tx.broadcast(executeTx);
+
+  if (isTxError(executeTxResult)) {
+    console.log("Mint failed.")
+    throw new Error(
+      `mint failed. code: ${executeTxResult.code}, codespace: ${executeTxResult.codespace}, raw_log: ${executeTxResult.raw_log}`
+    );
+  }
+
+  return executeTxResult;
+}
