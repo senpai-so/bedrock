09963524ac2a6a993fca61707bdb951ef2748c48
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-01-13 18:09:13 -0600

    Cleanup

--- contracts/rest-nft/packages/rest-nft/src/state.rs
+++ contracts/rest-nft/packages/rest-nft/src/state.rs
@@ -1,8 +1,6 @@
-// use cosmwasm_std::Empty;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-// pub type RestNFTContract<'a> = cw721_base::Cw721Contract<'a, Extension, Empty>;
 pub type Extension = Option<Metadata>;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
