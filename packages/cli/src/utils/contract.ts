63a2fa47158474d2133ad8de921ce7ae89d22d8e
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-03-26 22:11:10 -0600

    add store code func

--- packages/cli/src/utils/contract.ts
+++ packages/cli/src/utils/contract.ts
@@ -1,6 +1,28 @@
-import { isTxError, LCDClient, MsgExecuteContract, Wallet } from "@terra-money/terra.js"
-import { ConnectedWallet } from "@terra-money/wallet-provider";
+import fs from 'fs';
 
+import { isTxError, LCDClient, MsgExecuteContract, MsgStoreCode, Wallet } from "@terra-money/terra.js"
+import { ConnectedWallet } from "@terra-money/wallet-provider";
+import { getClient } from '../lib/getClient';
+import { encryptedToRawKey } from './keys';
+
+
+const storeCode = async (env: string, pk: string, pass: string) => {
+  // Load user creds
+  const terra = await getClient(env);
+  const key = encryptedToRawKey(pk, pass);
+  const wallet = terra.wallet(key);
+
+  const wasm = fs.readFileSync('../../contracts/bedrock/bedrock_base.wasm').toString('base64');
+  const storeCode = new MsgStoreCode(wallet.key.accAddress, wasm);
+  const storeCodeTx = await wallet.createAndSignTx({ msgs: [storeCode] });
+  const txResult = await terra.tx.broadcast(storeCodeTx)
+
+  if (isTxError(txResult)) throw new Error("store code failed");
+  const { store_code: { code_id } } = txResult.logs[0].eventsByType;
+  console.log(env, code_id);
+  
+  return code_id;
+}
 
 export const executeTransaction = async (
   terra: LCDClient,
