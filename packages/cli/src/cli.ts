8a3e9a6b4d2f0b6ed94904b17a1eb18e3e26c27e
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-03-27 17:59:34 -0600

    Add migrate command to CLI

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -2,6 +2,7 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { migrate } from './commands/migrate';
 
 import { mint } from './commands/mint';
 import { transfer } from './commands/transfer';
@@ -26,13 +27,13 @@ const argv = yargs(hideBin(process.argv))
         type: "string",
         alias: "k",
         demandOption: true,
-        description: "Private key from Terra Station"
+        description: "The ecnrytped private key from Terra Station"
       },
       pass: {
         type: "string",
         alias: "p",
         demandOption: true,
-        description: "Password for Terra Station"
+        description: "The password for your Terra Station wallet"
       },
       config: {
         type: "string",
@@ -65,13 +66,13 @@ const argv = yargs(hideBin(process.argv))
         type: "string",
         alias: "k",
         demandOption: true,
-        description: "Private key from Terra Station"
+        description: "The ecnrytped private key from Terra Station"
       },
       pass: {
         type: "string",
         alias: "p",
         demandOption: true,
-        description: "Password for Terra Station"
+        description: "The password for your Terra Station wallet"
       },
     })
   })
@@ -88,25 +89,71 @@ const argv = yargs(hideBin(process.argv))
         type: "string",
         alias: "k",
         demandOption: true,
-        description: "Private key from Terra Station"
+        description: "The ecnrytped private key from Terra Station"
       },
       pass: {
         type: "string",
         alias: "p",
         demandOption: true,
-        description: "Password for Terra Station"
+        description: "The password for your Terra Station wallet"
       },
       recipient: {
         type: "string",
         alias: "r",
         demandOption: true,
-        description: "Private key from Terra Station"
+        description: "Address of the recipient"
       },
       token_id: {
         type: "string",
         alias: "t",
         demandOption: true,
-        description: "Password for Terra Station"
+        description: "ID of the token to transfer"
+      },
+    })
+  })
+  .command("migrate", "migrate contract to a new code id", yargs => {
+    yargs.options({
+      env: { 
+        type: "string",
+        alias: "e",
+        demandOption: true,
+        description: "Chain environment",
+        choices: ["local", "testnet", "mainnet"],
+      },
+      key: {
+        type: "string",
+        alias: "k",
+        demandOption: true,
+        description: "The ecnrytped private key from Terra Station"
+      },
+      pass: {
+        type: "string",
+        alias: "p",
+        demandOption: true,
+        description: "The password for your Terra Station wallet"
+      },
+      code_id: {
+        type: "number",
+        alias: "c",
+        demandOption: true,
+        description: "The new code's storage ID"
+      },
+      contract_address: {
+        type: "string",
+        alias: "a",
+        demandOption: true,
+        description: "The address of the contract to update"
+      },
+      version: {
+        type: "string",
+        alias: "v",
+        demandOption: true,
+        description: "The new version for the contract"
+      },
+      config_path: {
+        type: "string",
+        demandOption: false,
+        description: "The path to the new contract configuration JSON file"
       },
     })
   })
@@ -116,31 +163,68 @@ const argv = yargs(hideBin(process.argv))
 const main = async () => {
   const args = await argv;
 
-  const command = args._[0];
-  const env = args.e as string | undefined;
-  const pk = args.k as string | undefined;
-  const pass = args.p as string | undefined;
-  const config = args.o as string | undefined;
-  const recipient = args.r as string | undefined;
-  const token_id = args.t as string | undefined;
-  const pinata_key = args.pinata_key as string | undefined;
-  const pinata_secret = args.pinata_secret as string | undefined;
+  const command = args._[0] as string;
+  const env = args.e as string;
+  const pk = args.k as string;
+  const pass = args.p as string;
 
   const cache = "cache";
 
-  if (typeof command === "string" && command === "upload") {
-    if (typeof env === 'undefined' || typeof pass === 'undefined' || typeof pk === 'undefined' || typeof pass === 'undefined' || typeof config === 'undefined' || typeof pinata_key === 'undefined' || typeof pinata_secret === 'undefined' ) return;
-    const path = args._[1] as string;
-    await upload(cache, env, path, pk, pass, config, pinata_key, pinata_secret);
-  } else if (typeof command === "string" && command === "mint") {
-    if (typeof env === 'undefined' || typeof pass === 'undefined' || typeof pk === 'undefined' || typeof pass === 'undefined') return;
-    await mint(env, pk, pass, cache);
-  } else if (typeof command === "string" && command === "transfer") {
-    if (typeof env === 'undefined' || typeof pass === 'undefined' || typeof pk === 'undefined' || typeof pass === 'undefined' || typeof config === 'undefined' || typeof recipient === 'undefined' || typeof token_id === 'undefined') return;
-    await transfer(env, pk, pass, cache, recipient, token_id);
-  } else {
-    console.error("Invalid command");
+  switch(command) {
+    case "upload":
+      const path = args._[1] as string;
+      const config = args.o as string;
+      const pinata_key = args.pinata_key as string;
+      const pinata_secret = args.pinata_secret as string;
+
+      await upload(cache, env, path, pk, pass, config, pinata_key, pinata_secret);
+
+      break;
+    case "mint":
+      await mint(env, pk, pass, cache);
+
+      break;
+    case "transfer":
+      const recipient = args.r as string;
+      const token_id = args.t as string;
+      
+      await transfer(env, pk, pass, cache, recipient, token_id);
+
+      break;
+    case "migrate":
+      const code_id = args.c as number;
+      const contract_address = args.a as string;
+      const version = args.v as string;
+      const config_path = args.config_path as string | undefined;
+
+      await migrate(env, pk, pass, code_id, contract_address, version, config_path);
+    
+      break;
+    default:
+      console.error("Invalid command");
   }
+
+  // if (command === "upload") {
+  //   const path = args._[1] as string;
+  //   const config = args.o as string;
+  //   const pinata_key = args.pinata_key as string;
+  //   const pinata_secret = args.pinata_secret as string;
+  //   await upload(cache, env, path, pk, pass, config, pinata_key, pinata_secret);
+  // } else if (command === "mint") {
+  //   await mint(env, pk, pass, cache);
+  // } else if (command === "transfer") {
+  //   const recipient = args.r as string;
+  //   const token_id = args.t as string;
+  //   await transfer(env, pk, pass, cache, recipient, token_id);
+  // } else if (command === "migrate") {
+  //   const code_id = args.c as number;
+  //   const contract_address = args.a as string;
+  //   const version = args.v as string;
+  //   const config_path = args.config_path as string | undefined;
+  //   await migrate(env, pk, pass, code_id, contract_address, version, config_path);
+  // } else {
+  //   console.error("Invalid command");
+  // }
   process.exit(0); // ensure smooth exit
 }
 
